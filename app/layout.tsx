df4cac045d9b2385150d3fab20b8f27951ce12ea
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2026-03-20 16:24:42 +0100

    fixed styling issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+		<html lang="en" className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 			<body>
 				<SmoothScrollProvider>{children}</SmoothScrollProvider>
 			</body>
